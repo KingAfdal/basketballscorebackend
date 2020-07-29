84e0c4e76f5f7473d9670ad221d9828b40dbede5
siubie <putraprima@gmail.com>
2020-07-29 09:53:47 +0700

    tambah gitignore

--- routes/api.php
+++ routes/api.php
@@ -7,7 +7,6 @@ use Illuminate\Support\Facades\Route;
 |--------------------------------------------------------------------------
 | API Routes
 |--------------------------------------------------------------------------
-|
 | Here is where you can register API routes for your application. These
 | routes are loaded by the RouteServiceProvider within a group which
 | is assigned the "api" middleware group. Enjoy building your API!
@@ -17,3 +16,9 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get("/",function (Request $request){
+    return "Hello World";
+});
+
+
